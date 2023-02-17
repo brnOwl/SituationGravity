2502448dd45631c76bafe794d59cc62d699c3ffb
lukeGallucci <luke@gallucci.us>
2023-02-17 14:28:58 -0600

    Environment and More Movement

---
+++ SituationGravity/Assets/Scripts/Climbing.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbing : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public PlayerController playerController; // rigidbody
+    public CharacterController controller;    // rigidbody
+    public LayerMask whatIsWall;
+
+    [Header("Climbing")]
+    public float climbSpeed;
+    public float maxClimbTime;
+    private float climbTimer;
+
+    private bool climbing;
+
+    [Header("ClimbJumping")]
+    public float climbJumpUpForce;
+    public float climbJumpBackForce;
+
+    // Input jump
+    //public KeyCode jumpKey = KeyCode.Space;
+    public int climbJumps;
+    private int climbJumpsLeft;
+
+    [Header("Detection")]
+    public float detectionLength;
+    public float sphereCastRadius;
+    public float maxWallLookAngle;
+    public float wallLookAngle;
+
+    private RaycastHit frontWallHit;
+    private bool wallFront;
+
+    private Transform lastWall;
+    private Vector3 lastWallNormal;
+    public float minWallNormalAngleChange;
+
+    void Update()
+    {
+        WallCheck();
+        StateMachine();
+
+        if (climbing) ClimbingMovement();
+    }
+
+    private void StateMachine()
+    {
+        // State 1 - Climbing
+        if (wallFront && playerController.isHorizontalMoving && wallLookAngle < maxWallLookAngle)
+        {
+            if (!climbing && climbTimer > .5) StartClimbing();
+
+            // Timer
+            if (climbTimer > 0) climbTimer -= Time.deltaTime;
+            if (climbTimer < 0) StopClimbing();
+        } 
+
+        // State 3 - None
+        else
+        {
+            if (climbing) StopClimbing();
+        }
+
+        if (wallFront && playerController.jumpAction.IsPressed() && climbJumpsLeft > 0) ClimbJump();
+    }
+
+    private void WallCheck()
+    {
+        wallFront = Physics.SphereCast(transform.position, sphereCastRadius, orientation.forward, out frontWallHit, detectionLength, whatIsWall);
+        // Define what angle the player can look to wall jump
+        wallLookAngle = Vector3.Angle(orientation.forward, -frontWallHit.normal);
+
+        // Determine if the next wall that's hit is a new wall - MAY DELETE LATER
+        bool newWall = frontWallHit.transform != lastWall || Mathf.Abs(Vector3.Angle(lastWallNormal, frontWallHit.normal)) > minWallNormalAngleChange;
+
+        // newWall - MAY DELETE LATER
+        if (controller.isGrounded || (wallFront && newWall))
+        {
+            climbTimer = maxClimbTime;
+            // Multiple Wall jumps
+            climbJumpsLeft = climbJumps;
+        }
+    }
+
+    private void StartClimbing()
+    {
+        climbing = true;
+        playerController.climbing = true;
+
+        lastWall = frontWallHit.transform;
+        lastWallNormal = frontWallHit.normal;
+
+        // camera fov change
+    }
+
+    private void ClimbingMovement()
+    {
+        playerController.jumpVelocity = new Vector3(playerController.jumpVelocity.x, climbSpeed, playerController.jumpVelocity.z);
+
+        // play sound effect
+    }
+
+    private void StopClimbing()
+    {
+        climbing = false;
+        playerController.climbing = false;
+        // particle effect
+    }
+
+    // Update is called once per frame
+    
+
+    private void ClimbJump()
+    {
+        Vector3 forceToApply = transform.up * climbJumpUpForce + frontWallHit.normal * climbJumpBackForce;
+
+        playerController.jumpVelocity = new Vector3(playerController.jumpVelocity.x, 0f, playerController.jumpVelocity.z);
+        playerController.jumpVelocity += forceToApply;
+
+        climbJumpsLeft--;
+    }
+
+
+}
--- SituationGravity/Assets/Scripts/PlayerController.cs
+++ SituationGravity/Assets/Scripts/PlayerController.cs
@@ -6,31 +6,76 @@ public class PlayerController : MonoBehaviour
     // Store the controls from the input system
     [Header("Input Objects")]
     [SerializeField] private PlayerInput playerInput;
-    [SerializeField] private InputAction jumpAction, moveAction, sprintAction;
+    [SerializeField] public InputAction jumpAction, moveAction, sprintAction;
+
     [Header("Player Movement Settings - Changeable")]
-    [SerializeField] private float playerSpeed = 6.0f;
+    [SerializeField] public float playerMoveSpeed = 6.0f;
+    [SerializeField] public float playerClimbingSpeed = 3.0f;
     [SerializeField] private float playerSprintSpeed = 12.0f;
     [SerializeField] private float playerAcceleration = 0.1f;
     [SerializeField] private float jumpForce = 1.0f;
     [SerializeField] private float doubleJumpForce = 2.0f;
     [SerializeField] private float gravityValue = -9.81f;
-    [Header("Observables")]
-    [SerializeField] private bool groundedPlayer = true;
-    [SerializeField] private bool canDoubleJump = false;
-    [SerializeField] private float controllerSpeed;
-    
 
-    
+
+    [Header("Observables")]
+    [SerializeField] public bool groundedPlayer = true;
+    [SerializeField] public bool canDoubleJump = false;
+    [SerializeField] public float controllerSpeed;
+    [SerializeField] public Vector3 playerDirection;
+    [SerializeField] public bool isHorizontalMoving = false;
 
     public CharacterController controller;
     public Transform camTransform;
-    private Vector3 playerVelocity;
+    public Vector3 jumpVelocity;
     
-    
-
     // Rotation Variables
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        wallrunning,
+        climbing,
+        crouching,
+        sliding,
+        air
+    }
+
+    public bool climbing;
+
+    private void StateHandler()
+    {
+        // Mode - Climbing
+        if (climbing && !groundedPlayer)
+        {
+            state = MovementState.climbing;
+            controllerSpeed = Mathf.Lerp(controllerSpeed, playerClimbingSpeed, playerAcceleration);
+        }
+
+        // Mode - Sprinting
+        else if (sprintAction.IsPressed() && groundedPlayer)
+        {
+            state = MovementState.sprinting;
+            controllerSpeed = Mathf.Lerp(controllerSpeed, playerSprintSpeed, playerAcceleration);
+        }
+
+        // Mode - Walking
+        else if (groundedPlayer)
+        {
+            state = MovementState.walking;
+            controllerSpeed = Mathf.Lerp(controllerSpeed, playerMoveSpeed, playerAcceleration);
+        }
+
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
+    }
 
     private void Awake()
     {
@@ -40,64 +85,78 @@ public class PlayerController : MonoBehaviour
         moveAction = playerInput.actions["Move"];
         sprintAction = playerInput.actions["Sprint"];
         controllerSpeed = 0;
-        
     }
 
     private void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < -1)
-        {
-            playerVelocity.y = -0.5f;
-            canDoubleJump = true;
-        }
+        // Player Movement Functions
+        GroundCheck();
+        PlayerMove();
+        PlayerJump();
+    }
 
-        // Player Movement Values
+    private void PlayerMove()
+    {
         Vector2 direction2 = moveAction.ReadValue<Vector2>();
         Vector3 direction = new Vector3(direction2.x, transform.position.y, direction2.y);
 
-        if (direction2.x != 0 || direction2.y != 0)
+        if (direction2.x != 0 || direction2.y != 0) isHorizontalMoving = true;
+        else isHorizontalMoving = false;
+
+        StateHandler();
+
+        if (isHorizontalMoving)
         {
             // Face the direction of movement
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            if (sprintAction.IsPressed()) controllerSpeed = Mathf.Lerp(controllerSpeed, playerSprintSpeed, playerAcceleration);
-            else controllerSpeed = Mathf.Lerp(controllerSpeed, playerSpeed, playerAcceleration);
-
             // Move the Player
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * controllerSpeed * Time.deltaTime);
+            playerDirection = moveDirection.normalized * controllerSpeed * Time.deltaTime;
+            controller.Move(playerDirection);
         }
         else
         {
             controllerSpeed = Mathf.Lerp(controllerSpeed, 0, playerAcceleration);
         }
-
-        PlayerJump();
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-
-
         // Output to Console
-        Debug.Log(direction);
-        if (jumpAction.triggered) Debug.Log("Hell yeah");
+        // Debug.Log(direction);
+        // if (jumpAction.triggered) Debug.Log("Hell yeah");
+        
+    }
+
+    private void GroundCheck()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && jumpVelocity.y < -1)
+        {
+            jumpVelocity.y = -0.5f;
+            canDoubleJump = true;
+        }
+        if (groundedPlayer)
+        {
+            jumpVelocity.x = 0;
+            jumpVelocity.z = 0;
+        }
     }
 
     private void PlayerJump()
     {
-        // JUMP
+        // Jump
         if (jumpAction.IsPressed() && groundedPlayer)
         {
-            playerVelocity.y = Mathf.Sqrt(jumpForce * -3.0f * gravityValue);
+            jumpVelocity.y = Mathf.Sqrt(jumpForce * -3.0f * gravityValue);
         }
-        // DOUBLE JUMP
+        // Double Jump
         else if (jumpAction.triggered && !groundedPlayer && canDoubleJump)
-        { 
-            playerVelocity.y = Mathf.Sqrt(doubleJumpForce * -3.0f * gravityValue);
+        {
+            jumpVelocity.y = Mathf.Sqrt(doubleJumpForce * -3.0f * gravityValue);
             canDoubleJump = false;
         }
+
+        jumpVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(jumpVelocity * Time.deltaTime);
     }
 }
